3b4292e2092b73eda9f945604c8ab830c777aa58
Michael Fuget <michaelfuget@gmail.com>
2021-02-14 18:36:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,7 +59,6 @@ function createTimeOutEvent(date)
 
 function hoursWorkedOnDate(date)
 {
-  console.log(this)
   let timeIn = this.cRecord.timeInEvents.find(x => x.date === date).hour
   let timeOut = this.cRecord.timeOutEvents.find(x => x.date === date).hour
 
